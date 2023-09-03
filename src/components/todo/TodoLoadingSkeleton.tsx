85c522f335b116e683934e0e48e75283555878a8
Slava Safonov <stray7777@gmail.com>
2023-09-03 14:59:14 -0300

    add margins to skeleton

--- src/components/todo/TodoLoadingSkeleton.tsx
+++ src/components/todo/TodoLoadingSkeleton.tsx
@@ -4,11 +4,11 @@ import { Card, Skeleton, Box } from "@mantine/core";
 const TodoSkeletonItem: React.FC = () => {
   return (
     <Card shadow="sm" padding="lg" radius="md" withBorder>
-      <Skeleton height={8} radius="xl" />
-      <Skeleton height={8} radius="xl" />
-      <Skeleton height={8} mt={6} radius="xl" />
-      <Skeleton height={8} mt={6} width="70%" radius="xl" />
-      <Skeleton height={50} circle mb="md" />
+      <Skeleton sx={MarginStyle} height={8} radius="xl" />
+      <Skeleton sx={MarginStyle} height={8} radius="xl" />
+      <Skeleton sx={MarginStyle} height={8} mt={6} radius="xl" />
+      <Skeleton sx={MarginStyle} height={8} mt={6} width="70%" radius="xl" />
+      <Skeleton sx={MarginStyle} height={50} circle mb="md" />
     </Card>
   );
 };
@@ -32,3 +32,8 @@ const TodoLoadingSkeleton: React.FC = () => {
 };
 
 export default TodoLoadingSkeleton;
+
+const MarginStyle = {
+  marginTop: "0.1rem",
+  marginBottom: "0.1rem",
+};
